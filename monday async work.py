b93ac2f7a32566bee9df3305463b294176d75a81
MaajXgeez <144708768+MaajXgeez@users.noreply.github.com>
2023-10-30 15:51:07 +0000

    monday  async work

---
+++ monday async work.py
@@ -0,0 +1,20 @@
+# 1. Identify 4 different data types and provide a 
+# use case/ scenario for when you would use each one.
+
+
+
+Integer (int):
+
+String (str):
+
+Floating-Point Number (float):
+Boolean (bool):
+
+# 2. Describe what a variable is and also provide 
+# three (3) variable naming rules
+
+Start with a Letter or Underscore: 
+
+Consist of Letters, 
+
+Case Sensitivity:
