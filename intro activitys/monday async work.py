e1f010f952518aa5c6147d6eed5412dcee6d78c3
MaajXgeez <144708768+MaajXgeez@users.noreply.github.com>
2023-11-03 16:06:06 +0000

    gitclone assighnment

--- intro activitys/monday async work.py
+++ intro activitys/monday async work.py
@@ -1,81 +1,20 @@
-o some research and find a piece of software that is built with Python. 
-The software can be a video game, a website, application, or new technology. Once you've found
-a program that was built with Python, conduct research create a report on what the software is,
-what it does, how the software was built, and most importantly how Python was used. 
-Your report should be atleast 700 words long. You will also need to provide
-citations for where you got your information - you may reference vidoes, 
-blogposts, websites, and articles. Please use MLA citation style.
-Your report will need atleast three (3) citations. 
-Your report will need to be submitted by the end of class.
-
-For your report, you may use a regular text file, just like how this file is formatted.
-text files are made using the .txt extnetion. 
-
-Example: iansReport.txt
-
-
-NO AI GENERATED RESOURCES/ LINKS 
-
-WHAT IS  MLA STYLE CITATION?
-MLA stands for Modern Language Association. As you go further in your academic, 
-and potentially professional career you'll need to conduct research and reference 
-where you got your information. 
-This is done for a variety of reasons, such as:
-- allows your reader to see where you got your information from.
-- gives credit to the people and organizations that have done the research.
-- protects the writer from claims of plagarizing their work. 
-
-MLA is written as followed:
-full name of the author, title of article/ video, date article was published, 
-website link for article/video\
+# 1. Identify 4 different data types and provide a 
+# use case/ scenario for when you would use each one.
 
 
 
+Integer (int):
 
+String (str):
 
+Floating-Point Number (float):
+Boolean (bool):
 
+# 2. Describe what a variable is and also provide 
+# three (3) variable naming rules
 
+Start with a Letter or Underscore: 
 
-I'm unable to create a text file for you, but I can provide you with a detailed report on a piece of software built with Python and include the necessary MLA citations. Let's discuss a popular software project developed with Python: Django.
+Consist of Letters, 
 
-**Title: Django - A Python-Powered Web Framework**
-
-**Introduction:**
-
-Django is a high-level, open-source web framework that is built using Python. It is known for its simplicity, robustness, and scalability. Django is widely used for web development due to its flexibility and the ease it offers to developers in creating web applications. This report will provide an overview of what Django is, its primary features, how it was built, and the role Python plays in this framework.
-
-**What is Django?**
-
-Django is a web framework that follows the Model-View-Controller (MVC) architectural pattern. It encourages rapid development and clean, pragmatic design. Released in 2005, Django has gained immense popularity within the web development community. It is suitable for building a wide range of web applications, from small projects to large, complex systems. Django comes equipped with various built-in features such as an Object-Relational Mapping (ORM) system, a templating engine, a built-in admin interface, and an authentication system. These features simplify the development process and enable developers to create robust web applications.
-
-**How Django Was Built:**
-
-Django was created by two experienced web developers, Adrian Holovaty and Simon Willison. It was initially developed to address the needs of the Lawrence Journal-World newspaper website. They wanted to create a framework that would enable them to build web applications quickly and efficiently. Django was built using Python, which is renowned for its readability and the simplicity it offers to developers. The developers behind Django aimed to leverage Python's strengths in their framework.
-
-**Python's Role in Django:**
-
-Python plays a fundamental role in Django's development and functionality. The following are some key ways in which Python is used in Django:
-
-1. **Code Readability:** Python's elegant and clear syntax contributes to Django's readability. Developers find it easier to write and understand code written in Python, making it a suitable language for building a user-friendly framework like Django.
-
-2. **Extensive Standard Library:** Python offers a rich standard library that simplifies various programming tasks. Django leverages Python's standard library for tasks like handling HTTP requests, database interactions, and more.
-
-3. **Object-Relational Mapping (ORM):** Django uses Python to create an ORM that allows developers to work with databases using Python objects. This simplifies database interactions and enables developers to focus on application logic rather than SQL queries.
-
-4. **Dynamic Typing:** Python's dynamic typing and duck typing allow developers to be more flexible when defining and using data structures, making it easier to work with various data types and structures within Django.
-
-5. **Community and Third-party Packages:** Python's large and active community has resulted in numerous third-party packages that can be seamlessly integrated into Django. These packages extend Django's capabilities, making it versatile for different web development needs.
-
-6. **Django's Core Code:** The majority of Django's core code, including the code for its built-in features like the templating engine, authentication system, and admin interface, is written in Python.
-
-**Citations:**
-
-1. Holovaty, Adrian, and Simon Willison. "The Web Framework for Perfectionists with Deadlines." DjangoProject.com. Django Project, 2005. [https://www.djangoproject.com/](https://www.djangoproject.com/)
-
-2. Grinberg, Miguel. "Flask vs. Django: Why Flask Might Be Better." miguelgrinberg.com. 2016. [https://blog.miguelgrinberg.com/post/flask-vs-django-why-flask-might-be-better](https://blog.miguelgrinberg.com/post/flask-vs-django-why-flask-might-be-better)
-
-3. Percival, Harry J.W. "Test-Driven Development with Python." O'Reilly Media, 2014.
-
-**Conclusion:**
-
-Django, a popular web framework built using Python, provides a powerful platform for web developers to create dynamic and feature-rich web applications. Python's role in Django is instrumental in making the framework both efficient and user-friendly. Its readability, extensive standard library, and the ability to create an ORM system contribute to Django's success in the web development industry. Django has been widely adopted for various web projects, from content management systems to e-commerce websites, and it continues to be a top choice for developers worldwide.
+Case Sensitivity:
---
+++ intro activitys/no 1st warmup.py
@@ -0,0 +1,5 @@
+1. 
+
+to arrange  software or hardware and to help keep track of the state of an application
+
+2. 1 give someone code, 2 how to make your own code, 3 telling you what code does.
