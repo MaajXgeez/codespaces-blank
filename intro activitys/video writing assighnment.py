caafd7030b4e9fcec194b6fce51781a31be2cffb
MaajXgeez <144708768+MaajXgeez@users.noreply.github.com>
2023-11-09 17:40:05 +0000

    video writing assighnment samaaj thompson.

---
+++ intro activitys/video writing assighnment.py
@@ -0,0 +1,15 @@
+What did the Jacquard loom do?
+Jacquard loom
+This handloom was used for weaving silk at Stonehouse in Lanarkshire in the 19th century. 
+It has a Jacquard attachment which allows complex patterns to be woven.
+The punch cards used in the Jacquard mechanism laid the foundation for modern computer programming.
+    to begin basicly this loom was one of the stepping stone of the  great horizion of coding it was what started it from the begining.
+
+another origin of computing in general is the abacus its like a nuber holder used mostly for the 
+counting of cattle it was a wooden grid of 4 rows with beads and you would use it to count 1s on the bottom 10s next 
+100s after that and 1000s after that and so fourth .
+
+it also said that machine code had a great affect of the entire coding world saying that 5–20 percent of coding tasks can now be automated with the help of AI. T
+his has had a significant impact on the labor market, reducing absolute demand for certain positions 
+while also increasing demand in new areas such as data science and machine learning.
+
